28bb288c720d14c9a7e6104960846a8175ca49e0
KevinAtwijuka <98968281+KevinAtwijuka@users.noreply.github.com>
2022-11-03 15:24:13 +0300

    kids name games

--- KIDS PROJECT/src/h/kids.java
+++ KIDS PROJECT/src/h/kids.java
@@ -6,35 +6,26 @@ import java.util.*;
 // REGNO: 2021/A/KIT/0748/F
 
 /** The kid enters the name and the program counts the letters in the name or word 
- * 2021/A/KIT/2021/F
+ * 2021/A/KIT/0748/F
  * Atwijuka Kevin
  *BIT
  */
 
-public class kids {
-	class Main {
-	  public static void main(String[] args) {
-	    Scanner myObj = new Scanner(System.in);  
-	    System.out.println("Enter Full Name:");
 
-	    String x = myObj.nextLine();  
-	    System.out.println("Full Name is " + x);  	    
-	    String string = "The best of both worlds";    
-	        int count = 0;    
-	            
+public class kids {}
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+System.out.print("Enter YOUR NAME: ");  
+String str= sc.nextLine();              //reads string   
+System.out.print("YOUR NAME IS: "+str); 
 
- 
-	        for(int i = 0; i < x.length(); i++) {    
-	            if(string.charAt(i) != ' ')    
-	                count++;    
-	        }    
-	            
-	         
-	        System.out.println("Total number of characters in a string: " + count);    
-	        
-	  }
-	}
-
-	}
+      int count = 0;
+     // System.out.println("String: "+str);
+      for (int i = 0; i < str.length(); i++) {
+         if (Character.isLetter(str.charAt(i)))
+         count++;
+      }
+      System.out.println(" \n Number of letters in your name =  "+count );
+    }
 
 
